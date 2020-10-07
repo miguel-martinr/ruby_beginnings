b59e3c2541706fde4e907f715a110060c0af4f45
Alejandro <alu0101209777@ull.edu.es>
2020-10-07 14:45:38 +0100

    colored triangles

--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
-def find_outlier(integers)
-  integers.partition(&:odd?).find(&:one?)[0]
+def add_colour(colors)
+  if colors.length == 2
+    case colors.split("").sort.join
+    when "BG"
+      return "R"
+    when "GR"
+      return "B"
+    when "BR"
+      return "G"
+    else 
+      return colors[0]
+    end
+  end
 end
 
-puts find_outlier([2,4,6,8,7])
+# R B R G B R B
+#  G G B R G G
+#   G R G B G
+#    B B R R
+#     B G R
+#      R B
+#       G
+
+def triangle(colors)
+
+  if colors.length == 1
+    return colors
+  end
+
+  while colors.length > 1
+    sep_colors = colors.split("").zip(colors.split("").slice(1,colors.length-1))
+    sep_colors.delete_at(-1)
+    sep_colors = sep_colors.map(&:join)
+    colors = ""
+    sep_colors.each {|x|
+      colors += add_colour(x)
+    }
+  end
+  colors
+end
+
+
+puts triangle("RBRGBRB")
